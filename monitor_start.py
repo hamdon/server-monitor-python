5292ff83b7c84f69a058d0582597339c213299a8
hamdon <cao4141@qq.com>
2017-03-28 18:53:27 +0800

    动态更新间隔更新的时间

--- monitor_start.py
+++ monitor_start.py
@@ -8,11 +8,11 @@ import time
 cf = ConfigParser.ConfigParser()
 CURRENT_DIR = os.path.dirname(__file__)
 ################################################
-# common config
-COMMON_FILE = os.path.abspath(os.path.join(CURRENT_DIR, "common.conf"))
-cf.read(COMMON_FILE)
-server_monitor_time_interval= cf.get("common", "server_monitor_time_interval")
 RUN_FILE=os.path.abspath(os.path.join(CURRENT_DIR, "monitor.py"))
 while True:
     os.system("python "+ RUN_FILE)
+    # common config
+    COMMON_FILE = os.path.abspath(os.path.join(CURRENT_DIR, "common.conf"))
+    cf.read(COMMON_FILE)
+    server_monitor_time_interval = cf.get("common", "server_monitor_time_interval")
     time.sleep(server_monitor_time_interval)
